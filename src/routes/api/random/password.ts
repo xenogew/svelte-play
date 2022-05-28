60285d14eac08589be184ac7f45f1ffb076be11e
WangAphisit Nattasan <ts-natta.wangaphisit@rakuten.com>
2022-05-29 03:34:32 +0900

    implement with reading env var

--- src/routes/api/random/password.ts
+++ src/routes/api/random/password.ts
@@ -1,6 +1,7 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get() {
-	const response = await fetch('http://127.0.0.1:8080/api/v1/gen/password/random');
+	const baseUrl = import.meta.env.VITE_RUST_API_URL;
+	const response = await fetch(`${baseUrl}/api/v1/gen/password/random`);
 	const jsonBody = await response.json();
 	return {
 		headers: {
--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter({
 			edge: false,
-			external: [],
+			external: ['@sveltejs/kit/node/polyfills'],
 			split: false
 		})
 	}
