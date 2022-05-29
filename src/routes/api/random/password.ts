63a109503793fe21b77c533762b19f44798ebc53
WangAphisit Nattasan <ts-natta.wangaphisit@rakuten.com>
2022-05-29 20:32:36 +0900

    update fetching url both self and api server

--- src/routes/api/random/password.ts
+++ src/routes/api/random/password.ts
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get() {
-	const baseUrl = import.meta.env.VITE_RUST_API_URL;
+	const baseUrl: string = import.meta.env.VITE_RUST_API_URL;
 	const response = await fetch(`${baseUrl}/api/v1/gen/password/random`);
 	const jsonBody = await response.json();
 	return {
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,18 +24,10 @@
 	let generatedPassword = '';
 	let toasting = false;
 
-	const handleSuccessfullyCopied = async (e: CustomEvent) => {
-		console.log(`successfully copied to clipboard! '${e.detail}'`);
-	};
-
-	const handleFailedCopy = async () => {
-		console.log('failed to copy :(');
-	};
-
 	const sleep = (m: number) => new Promise((r: Function) => setTimeout(r, m));
 
 	const fetchRandomPassword = async () => {
-		const response = await fetch('http://localhost:3000/api/random/password');
+		const response = await fetch('/api/random/password');
 		const jsonBody = await response.json();
 		generatedPassword = jsonBody.pw;
 	};
@@ -48,7 +40,7 @@
 
 	<Header />
 
-	<form>
+	<form on:submit|preventDefault={fetchRandomPassword}>
 		<div class="grid grid-cols-1 gap-x-6 gap-y-3 sm:gap-6 mt-4 sm:grid-cols-4 py-6">
 			<div class="text-left my-0 sm:my-auto sm:text-right cursor-default">
 				<span id="pw-length" class="text-gray-700 dark:text-gray-200">Password Length</span>
@@ -130,7 +122,7 @@
 		<div class="flex justify-center mt-6">
 			<button
 				class="px-6 py-2 leading-5 w-full text-white text-lg font-medium transition-colors duration-200 ease-in-out transform bg-blue-700 hover:bg-blue-800 focus:bg-blue-800 shadow-md hover:shadow-lg rounded-md focus:outline-none"
-				on:click|preventDefault={fetchRandomPassword}>Generate</button
+				>Generate</button
 			>
 		</div>
 
@@ -150,8 +142,8 @@
 
 			<CopyToClipboard
 				text={generatedPassword}
-				on:copy={handleSuccessfullyCopied}
-				on:fail={handleFailedCopy}
+				on:copy={async () => console.log(`successfully copied to clipboard! 😎`)}
+				on:fail={async () => console.log('failed to copy 😣')}
 				let:copy
 			>
 				<button
