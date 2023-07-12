a68ef1a98e401ce2dc3956fb9a14db1a9f6ad6b7
WangAphisit Nattasan <ts-wangaphi.nattasan@rakuten.com>
2023-07-13 01:03:50 +0900

    update playwright config and test suite

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,11 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 3000
-	}
+		command: 'pnpm run build && pnpm run preview --host',
+		port: 4173
+	},
+	testDir: 'tests',
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 };
 
 export default config;
